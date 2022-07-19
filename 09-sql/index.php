a3e8a3aad41c69d999d3579952bba0504940dac7
Matthieu Mota <matthieumota@gmail.com>
2022-07-19 10:07:58 +0200

    Movie list

--- 09-sql/index.php
+++ 09-sql/index.php
@@ -1,11 +1,50 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDO PHP Webflix</title>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+</head>
+<body>
+    <?php
+        require 'config/db.php';
 
-require 'config/db.php';
+        // On va écrire une requête SQL dans le PHP
+        $query = $db->query('SELECT * FROM movie');
 
-// On va écrire une requête SQL dans le PHP
-$query = $db->query('SELECT * FROM movie');
+        // On exécute la requête pour avoir un tableau avec tous les films
+        $movies = $query->fetchAll(); // Tableau qu'on va parcourir...
 
-// On exécute la requête pour avoir un tableau avec tous les films
-$movies = $query->fetchAll(); // Tableau qu'on va parcourir...
+        // var_dump($movies);
 
-var_dump($movies);
+        function truncate($text, $ending = '...') {
+            if (strlen($text) > 15) {
+                return substr($text, 0, 15).$ending;
+            }
+
+            return $text;
+        }
+    ?>
+
+    <div class="max-w-5xl mx-auto">
+        <h1 class="text-center text-xl">Les films</h1>
+
+        <div class="flex flex-wrap">
+            <?php foreach ($movies as $movie) { ?>
+                <div class="w-1/2 md:w-1/3 lg:w-1/4">
+                    <div class="border rounded-lg shadow m-3">
+                        <div class="p-3">
+                            <h2><?= truncate($movie['title']); ?></h2>
+                            <p class="text-xs text-gray-400"><?= substr($movie['released_at'], 0, 4); ?></p>
+                        </div>
+                        <?php if ($movie['cover']) { ?>
+                        <img class="h-[300px] w-full object-cover rounded-b" src="img/<?= $movie['cover']; ?>" alt="<?= $movie['title']; ?>">
+                        <?php } ?>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</body>
+</html>
