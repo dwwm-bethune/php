8ed47f50cd5f5a6dc70c6c81f9ffee3118264100
Matthieu Mota <matthieumota@gmail.com>
2022-07-19 09:07:49 +0200

    DB in require

--- 09-sql/index.php
+++ 09-sql/index.php
@@ -1,11 +1,7 @@
 <?php
 
-// PHP se connecte au SQL
-$db = new PDO('mysql:host=localhost;port=3306;dbname=webflix', 'root', '', [
-    // Paamayim Nekudotayim => Opérateur de résolution de portée ::
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // On veut un tableau associatif en résultat
-]);
-http://localhost/php/09-sql/
+require 'config/db.php';
+
 // On va écrire une requête SQL dans le PHP
 $query = $db->query('SELECT * FROM movie');
 
